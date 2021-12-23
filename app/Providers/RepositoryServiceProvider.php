ae307fe9b417a9f33d44140a41813bb71f000b8c
Jones Heckler <mw10@mw10.com.br>
2021-12-23 14:39:18 -0300

    registro do repositor

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+
+use App\Repositories\Contracts\{
+    UserRepositoryInterface,
+    
+};
+use App\Repositories\Core\Eloquent\{
+    EloquentUserRepository,
+
+};
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            UserRepositoryInterface::class,
+            EloquentUserRepository::class
+        );
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
