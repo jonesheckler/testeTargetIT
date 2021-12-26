13a2cd2254fe123f09113fb48d3e457346562cde
Jones Heckler <mw10@mw10.com.br>
2021-12-26 20:58:24 -0300

    ajuste na rota

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,18 +18,22 @@ use Illuminate\Support\Facades\Route;
 
 
 
-// Route::get('/', function(){
-//     return response('OK - Funcionando', 200);
-// });
+Route::get('/', function(){
+    return response('OK - Funcionando', 200);
+});
 
 
 
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('apiJwt')->group(function () {
     Route::resource('/user', UserApiController::class);
 });
 
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::prefix('v1')->middleware('ApiJwtAdmin')->group(function () {
+    Route::get('/all_user', [UserApiController::class, 'all']);
+});
+
+
+Route::post('login', [AuthController::class, 'login']);
+
